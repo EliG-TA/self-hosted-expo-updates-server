c282302c657e8c743f50a114a9c384e550b64934
Genrikh Bogatskii <darwim@yandex.ru>
2025-12-09 13:08:41 +0100

    feat(ui): add version grouping and sorting to published updates
    Group published updates by version, sort versions in descending order,
    and sort updates within each version by release date descending. This
    improves the organization and readability of the updates list.

--- Web/src/Pages/App/PublishedUpdates.jsx
+++ Web/src/Pages/App/PublishedUpdates.jsx
@@ -1,17 +1,46 @@
 import { Text, Card, Spinner } from '../../Components'
 import { useCQuery } from '../../Services'
 import { UpdateInfo } from './UpdateInfo'
+import _ from 'lodash'
+
+const compareVersions = (a, b) => {
+  const partsA = a.split(/[-.]/).map(x => isNaN(x) ? x : parseInt(x))
+  const partsB = b.split(/[-.]/).map(x => isNaN(x) ? x : parseInt(x))
+  for (let i = 0; i < Math.max(partsA.length, partsB.length); i++) {
+    const partA = partsA[i] || 0
+    const partB = partsB[i] || 0
+    if (partA > partB) return 1
+    if (partA < partB) return -1
+  }
+  return 0
+}
 
 export const PublishedUpdates = ({ app }) => {
   const { data: published, isSuccess } = useCQuery(['published', app._id])
 
   if (!isSuccess) return <Spinner />
+
+  // Group by version
+  const grouped = _.groupBy(published, 'version')
+
+  // Sort versions descending
+  const sortedVersions = Object.keys(grouped).sort((a, b) => compareVersions(b, a))
+
+  // Sort updates within each group by releasedAt descending
+  sortedVersions.forEach(version => {
+    grouped[version] = _.sortBy(grouped[version], u => -new Date(u.releasedAt))
+  })
+
   return (
     <Card title='PUBLISHED UPDATES' collapsable collapsed={!published.length} fadeIn style={{ padding: 20, width: '100%', maxWidth: 900, marginTop: 40 }}>
       {!published.length && <Text value='No published updates yet, upload and release one to see it here' />}
-      {published.map((update, ind) => (
-        <Card key={update._id} collapsable collapsed={!!ind} title={`${update.version} - ${update.releaseChannel} - ${update.gitCommit}`} style={{ marginTop: 20 }}>
-          <UpdateInfo update={update} />
+      {sortedVersions.map((version, versionInd) => (
+        <Card key={version} collapsable collapsed={!!versionInd} title={`Version ${version}`} style={{ marginTop: 20 }}>
+          {grouped[version].map((update, ind) => (
+            <Card key={update._id} collapsable collapsed={!!ind} title={`${update.releaseChannel} - ${update.gitCommit}`} style={{ marginTop: 10 }}>
+              <UpdateInfo update={update} />
+            </Card>
+          ))}
         </Card>
       ))}
     </Card>
